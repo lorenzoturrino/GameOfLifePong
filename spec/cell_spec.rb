2f19c01ae2cdafd2f15eb056023a6a4b1c4edbeb
LorenzoTurrino <lorenzo.turrino@gmail.com>
2016-05-20 16:57:07 +0100

    testy

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -1,6 +1,7 @@
 require 'cell'
 describe Cell do
   subject(:test_cell) {described_class.new}
+  let(:dummy_friends) {Array.new(8) {described_class.new}}
 
   describe '#alive' do
     it 'has a default dead status' do
@@ -10,7 +11,14 @@ describe Cell do
 
   describe '#friends' do
     it 'returns an array of close cells' do
-      expect(test_cell.alive[0].class).to_eq test_cell.class
+      expect(test_cell.friends[0].class).to_eq test_cell.class
+    end
+  end
+
+  describe '#set_friends' do
+    it 'accepts an array of close cells and save it to friends' do
+      test_cell.set_friends(dummy_friends)
+      expect(test_cell.friends).to eq dummy_friends
     end
   end
 
--- spec/game_master_spec.rb
+++ spec/game_master_spec.rb
@@ -1,10 +1,19 @@
 describe GameMaster do
-  subject(:test_game_master) {described_class.new 10, 10}
+  subject(:test_game_master) {described_class.new 10, 10, dummy_cell_class}
+  let(:dummy_cell) {double :cell}
+  let(:dummy_cell_class) {double :cell_class, new: dummy_cell}
 
   describe '#grid' do
     it 'is a grid of the size specified at creation. yes sucky test too bad for you' do
       expect(test_game_master.grid.size).to eq 10
       expect(test_game_master.grid[0].size).to eq 10
+    end
+  end
+
+  describe '#populate' do
+    it 'fills the grid with new instances of cell' do
+      test_game_master.populate
+      expect(test_game_master.grid[0][0].class).to eq dummy_cell_class
     end
   end
 
